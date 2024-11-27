32d9ea745f067ea1bf23606b946cbc19ff8f9c0d
codingtaxipj <prayasjadli18@gmail.com>
2024-11-27 18:41:57 +0530

    grid-background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
-
 function App() {
-  
-
   return (
     <>
-     <div>testing the remote repo feature</div>
+      <div className="h-screen w-screen bg-[url('./assets/dark-blue.jpg')] ">
+        <div className="absolute z-0 h-screen w-screen bg-[linear-gradient(to_right,_rgba(255,255,255,0.03)_1px,_transparent_1px),linear-gradient(to_bottom,_rgba(255,255,255,0.03)_1px,_transparent_1px)] [background-size:25px_25px]">
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
