0db1942b4a0493b43c5699daea30931bf7418dd8
codingtaxipj <prayasjadli18@gmail.com>
2024-12-24 04:56:07 +0530

    add progress bard on home dashboard | fixed on click change css of side nav buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import HomeDashboard from "./components/HomeDashboard";
 import IncomeDashboard from "./components/IncomeDashboard";
 import ExpenseDashboard from "./components/ExpenseDashboard";
@@ -6,12 +6,27 @@ import AllCategories from "./components/AllCategories";
 
 function App() {
   const [defaultDashboard, setDefaultDashboard] = useState("homeDashboard");
+  const home = useRef();
+
+  function handleClickSideButtons(pageSelected) {
+    setDefaultDashboard(pageSelected);
+  }
+
+  function setStyle(current) {
+    let baseStyle = "w-full py-2 text-left text-[24px] font-medium outline-0";
+    if (current === defaultDashboard)
+      return baseStyle + " " + "rounded-l-[12px] bg-[white] px-5 text-[black]";
+    else
+      return (
+        baseStyle + " " + "text-slate-600 px-2 hover:px-5 hover:text-[white]"
+      );
+  }
 
   return (
     <>
       <div className="absolute inset-0 w-full bg-pupl bg-cover bg-no-repeat">
         <div className="h-full w-full bg-[linear-gradient(to_right,_rgba(0,0,0,0.2)_1px,_transparent_1px),linear-gradient(to_bottom,_rgba(0,0,0,0.2)_1px,_transparent_1px)] px-[5rem] py-20 [background-size:25px_25px]">
-          <div className="absolute inset-x-[8rem] inset-y-[6rem] flex flex-row rounded-[20px] bg-[black] px-8 py-8">
+          <div className="inset-x-[8rem] inset-y-[6rem] flex h-full flex-row rounded-[20px] bg-[black] px-8 py-8">
             <div className="w-[22rem] pl-10">
               <div className="py-[2.5rem] text-[white]">
                 <div className="mb-5 size-[5rem] rounded-[12px] bg-pupl"></div>
@@ -22,33 +37,34 @@ function App() {
               </div>
               <div className="flex flex-col py-[2rem] font-pop-b text-[#979797]">
                 <button
-                  onClick={() => setDefaultDashboard(() => "homeDashboard")}
+                  ref={home}
+                  onClick={() => handleClickSideButtons("homeDashboard")}
                   autoFocus
-                  className="text-slate-600 w-full px-2 py-2 text-left text-[24px] font-medium outline-0 hover:px-5 hover:text-[white] focus:rounded-l-[12px] focus:bg-[white] focus:px-5 focus:text-[black]"
+                  className={setStyle("homeDashboard")}
                 >
                   Dashboard
                 </button>
                 <button
-                  onClick={() => setDefaultDashboard(() => "expenseDashboard")}
-                  className="text-white w-full px-2 py-2 text-left text-[24px] font-medium hover:px-5 hover:text-[white] focus:rounded-l-[12px] focus:bg-[white] focus:px-5 focus:text-[black]"
+                  onClick={() => handleClickSideButtons("expenseDashboard")}
+                  className={setStyle("expenseDashboard")}
                 >
                   Expenses
                 </button>
                 <button
-                  onClick={() => setDefaultDashboard(() => "incomeDashboard")}
-                  className="text-white w-full px-2 py-2 text-left text-[24px] font-medium hover:px-5 hover:text-[white] focus:rounded-l-[12px] focus:bg-[white] focus:px-5 focus:text-[black]"
+                  onClick={() => handleClickSideButtons("incomeDashboard")}
+                  className={setStyle("incomeDashboard")}
                 >
                   Income
                 </button>
                 <button
-                  onClick={() => setDefaultDashboard(() => "AllCategories")}
-                  className="text-white w-full px-2 py-2 text-left text-[24px] font-medium hover:px-5 hover:text-[white] focus:rounded-l-[12px] focus:bg-[white] focus:px-5 focus:text-[black]"
+                  onClick={() => handleClickSideButtons("AllCategories")}
+                  className={setStyle("AllCategories")}
                 >
                   All Categories
                 </button>
               </div>
             </div>
-            <div className="w-full rounded-[20px] bg-[white]">
+            <div className="h-full w-full rounded-[20px] bg-[white]">
               {defaultDashboard === "homeDashboard" && <HomeDashboard />}
               {defaultDashboard === "incomeDashboard" && <IncomeDashboard />}
               {defaultDashboard === "expenseDashboard" && <ExpenseDashboard />}
--- src/components/HomeDashboard.jsx
+++ src/components/HomeDashboard.jsx
@@ -1,17 +1,16 @@
 import dots from "../assets/4dots.svg";
 import CircleIcon from "./CircleIcon";
-import { PiDotOutlineFill } from "react-icons/pi";
+import { PiDotOutlineFill, PiTagSimpleFill } from "react-icons/pi";
 import dummyData from "../assets/lib/demoData.json";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 
 const DummyEntries = JSON.parse(JSON.stringify(dummyData));
-console.log("gg is" + dummyData[4].userGivenCategory);
 
 const HomeDashboard = () => {
   return (
     <>
-      <div className="flex flex-row gap-2">
-        <div className="w-[70%] px-[4rem] py-[2.5rem]">
+      <div className="flex h-full flex-row">
+        <div className="bg-white w-[70%] rounded-[20px] px-[4rem] py-[2.5rem]">
           <div className="flex flex-row items-center justify-start pb-2">
             <img className="size-[2rem]" src={dots} alt="dot-icon" />
             <h4 className="pl-2 font-pop-b text-[24px] text-black">
@@ -36,26 +35,22 @@ const HomeDashboard = () => {
                     {data.primeCategory}
                   </p>
                   <p className="flex items-center gap-2 font-pop-m text-[14px]">
-                    {" "}
-                    <span className="rounded-full bg-travel p-[3px]"></span>{" "}
+                    <span className="rounded-full bg-travel p-[3px]"></span>
                     {data.subCategory}
                   </p>
                   {data.userGivenCategory && (
                     <p className="flex items-center gap-2 font-pop-m text-[14px]">
-                      {" "}
                       <span className="rounded-full bg-food p-[3px]"></span>
                       {data.userGivenCategory}
                     </p>
                   )}
                 </div>
                 <div className="flex items-center gap-2 font-pop-r text-[14px] text-[#919191]">
-                  {" "}
-                  <span>{data.entryTime}</span>{" "}
+                  <span>{data.entryTime}</span>
                   <span>
-                    {" "}
-                    <PiDotOutlineFill />{" "}
-                  </span>{" "}
-                  <span>{data.entryTitle}</span>{" "}
+                    <PiDotOutlineFill />
+                  </span>
+                  <span>{data.entryTitle}</span>
                 </div>
               </div>
               <div className="flex flex-row items-center pr-2 font-pop-sb text-[20px] text-[black]">
@@ -66,10 +61,54 @@ const HomeDashboard = () => {
               </div>
             </div>
           ))}
-
-          <div className="m-2 mb-5 flex flex-row items-center gap-2 px-[.75rem] py-[1rem]"></div>
         </div>
-        <div className="w-[30%] grow rounded-r-md bg-[#cfcfcf]">side bar</div>
+        <div className="h-full w-[30%] rounded-r-[20px] bg-[#f3f4f6]">
+          <div className="flex h-full flex-col justify-center gap-10 px-14">
+            <div>
+              <h6 className="inline-flex items-center gap-2 font-pop-sb text-[16px] text-black">
+                <PiTagSimpleFill className="text-pupl" />
+                How much you spent ?
+              </h6>
+
+              {[1, 2, 3, 4, 5].map((data) => (
+                <div key={data}>
+                  <div className="flex grow flex-row gap-2 pb-1 pt-4 font-pop-m text-[14px]">
+                    <p className="w-full">Utlities & Bills</p>
+                    <p className="flex items-center text-right font-pop-sb ">
+                      <FaIndianRupeeSign /> <span>20</span>
+                    </p>
+                  </div>
+                  <div className="h-1.5 w-full rounded-full bg-[#d1d5db]">
+                    <div className="h-full w-[45%] rounded-full bg-pupl"></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div>
+              <h6 className="font-pop-sb inline-flex items-center gap-2 text-[16px] text-black">
+              < PiTagSimpleFill className="text-pupl" /> How much you earned ?
+              </h6>
+
+              {[1, 2, 3, 4, 5].map((data) => (
+                <div key={data}>
+                  <div className="flex grow flex-row gap-2 pb-1 pt-4 font-pop-m text-[14px]">
+                    <p className="w-full">Utlities & Bills</p>
+                    <p className="flex items-center text-right font-pop-sb">
+                      <FaIndianRupeeSign /> <span>20</span>
+                    </p>
+                  </div>
+                  <div className="h-1.5 w-full rounded-full bg-[#d1d5db]">
+                    <div className="h-full w-[45%] rounded-full bg-pupl"></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="inline-flex gap-5 justify-center" >
+              <button className="bg-pupl text-[white] px-4 py-1 rounded-full" > Add Expence </button>
+              <button className="bg-pupl text-[white] px-4 py-1 rounded-full" > Add Income </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
