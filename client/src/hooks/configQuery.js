339a712cd567350be3c42d41ef60f1cc65e03ec5
Prayas Jadli <prayasjadli18@gmail.com>
2025-04-29 16:25:18 +0530

    query logic update

--- client/src/hooks/configQuery.js
+++ client/src/hooks/configQuery.js
@@ -1,19 +1,37 @@
- import {  } from "react"
-  import { useQuery } from "@tanstack/react-query"
-   import axios from "axios"
-export const useQueryToFetch = () =>{
+import {} from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
-        const fetchData  = async () => await axios.get("http://localhost:8080/expense/get-data");      
-           
-        const {data,isLoading} = useQuery({
-                queryKey : ['expense'],
-                querryFn : fetchData,
-            });      
+export const useQueryToFetch = () => {
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8080/expense/get-data");
+    return response.data;
+  };
 
-        if (isLoading) return <div>loading...</div>
-        console.log(data);
-        
-        if (!isLoading) return data ;
-        }
+  return useQuery({
+    queryKey: ["expense"],
+    queryFn: fetchData,
+  });
+};
 
-export const useQueryToUpdate = () =>{}
+export const useQueryToUpdate = () => {
+  const pushData = async data => {
+    const response = await axios.post(
+      "http://127.0.0.1:8080/expense/add-data",
+      data
+    );
+    return response.data;
+  };
+
+  return useMutation({
+    mutationFn: pushData,
+    onSuccess: data => {
+      console.log("Expense added:", data);
+      alert("Expense added successfully!");
+    },
+    onError: error => {
+      console.error("Error adding expense:", error);
+      alert("Something went wrong.");
+    },
+  });
+};
