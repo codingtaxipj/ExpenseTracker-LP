2ea3e5f65e911a3d2396206e1880821bc9a2a565
codingtaxipj <prayasjadli18@gmail.com>
2025-05-13 01:05:07 +0530

    file left to add in last commit

--- client/src/components/utility.js
+++ client/src/components/utility.js
@@ -1,12 +1,12 @@
 import moment from "moment";
 
-export const sortByYear = (entries, year) => {
-  const thisEntries = entries.filter(
+export const sortByYearAsMonths = (entries, year) => {
+  const list = entries.filter(
     (items) => moment(items.entryDate).year() === year,
   );
   const stack = [];
   for (let i = 0; i <= 11; i++) {
-    const total = thisEntries
+    const total = list
       .filter((items) => moment(items.entryDate).month() === i)
       .reduce((sum, items) => sum + items.amount, 0);
     stack.push({
@@ -17,31 +17,68 @@ export const sortByYear = (entries, year) => {
   return stack;
 };
 
-export const sortByMonth = (entries, month, filterBy = "week") => {
-  const thisEntries = entries.filter(
+export const sortByMonthAsWeeks = (entries, month) => {
+  const list = entries.filter(
     (items) => moment(items.entryDate).month() === month,
   );
-  const monthDate = [];
+
   const stack = [];
-  const startBy = moment().month(month).startOf("month");
-  const endBy = moment().month(month).endOf("month");
+  const startBy = moment().month(month).startOf("month").week();
+  const endBy = moment().month(month).endOf("month").week();
 
-  monthDate.startWeek = startBy.week();
-  monthDate.endWeek = endBy.week();
-
-  if (filterBy === "week") {
-    monthDate.startDay = startBy.date();
-    monthDate.endDay = endBy.date();
-    for (let i = monthDate.startWeek; i <= monthDate.endWeek; i++) {
-      const total = thisEntries
-        .filter((items) => moment(items.entryDate).week() === i)
-        .reduce((sum, items) => sum + items.amount, 0);
-      stack.push({
-        Title: moment().day(i).format("DD"),
-        Amount: total,
-      });
-    }
+  for (let i = startBy; i <= endBy; i++) {
+    const total = list
+      .filter((items) => moment(items.entryDate).week() === i)
+      .reduce((sum, items) => sum + items.amount, 0);
+    stack.push({
+      Title: moment().week(i).format("wo") + " Week",
+      Amount: total,
+    });
   }
 
+  return stack;
+};
+
+export const sortByMonthAsDates = (entries, month) => {
+  const list = entries.filter(
+    (items) => moment(items.entryDate).month() === month,
+  );
+
+  const stack = [];
+  const startBy = moment().month(month).startOf("month").date();
+  const endBy = moment().month(month).endOf("month").date();
+
+  for (let i = startBy; i <= endBy; i++) {
+    const total = list
+      .filter((items) => moment(items.entryDate).date() === i)
+      .reduce((sum, items) => sum + items.amount, 0);
+    stack.push({
+      Title: moment().date(i).format("DD"),
+      Amount: total,
+    });
+  }
+
+  return stack;
+};
+
+export const sortByWeekAsDates = (entries, week) => {
+  const list = entries.filter(
+    (items) => moment(items.entryDate).week() === week,
+  );
+  const stack = [];
+  const startBy = moment().week(week).startOf("week").date();
+  const endBy = moment().week(week).endOf("week").date();
+  for (let i = startBy; i <= endBy; i++) {
+    const total = list
+      .filter((items) => moment(items.entryDate).date() === i)
+      .reduce((sum, items) => sum + items.amount, 0);
+    stack.push({
+      Title:
+        moment().date(i).format("Do") +
+        " " +
+        moment().week(week).startOf("week").format("MMM"),
+      Amount: total,
+    });
+  }
   return stack;
 };
