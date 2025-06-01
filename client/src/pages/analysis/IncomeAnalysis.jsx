02e91b9a5b7bd119f94a89a15027ebf4add4d43f
codingtaxipj <prayasjadli18@gmail.com>
2025-06-02 02:46:55 +0530

    minor

--- client/src/pages/analysis/IncomeAnalysis.jsx
+++ client/src/pages/analysis/IncomeAnalysis.jsx
@@ -2,7 +2,6 @@ import NavMenu from "@/components/Navigation/NavMenu";
 import { PATH } from "@/router/routerConfig";
 import YearByYearComparision from "@/components/analysis/YearByYearComparision";
 import YearlyAnalysis from "@/components/analysis/YearlyAnalysis";
-import PrimeCategoryYearlyAnalysis from "@/components/analysis/PrimeCategoryYearlyAnalysis";
 import SubCategoryMonthlyAnalysis from "@/components/analysis/SubCategoryMonthlyAnalysis";
 
 const IncomeAnalysis = () => {
@@ -10,9 +9,11 @@ const IncomeAnalysis = () => {
     <div>
       <NavMenu activeBtn={PATH.incomeAnalysis}>
         <div className="bg-darkBlack [&::-webkit-scrollbar-track]:bg-grey-border [&::-webkit-scrollbar-thumb]:bg-grey-hover w-full overflow-y-auto p-10 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:rounded-full">
-          <YearlyAnalysis></YearlyAnalysis>
-          <YearByYearComparision></YearByYearComparision>
-          <SubCategoryMonthlyAnalysis></SubCategoryMonthlyAnalysis>
+          <YearlyAnalysis isExpense={false}></YearlyAnalysis>
+          <YearByYearComparision isExpense={false}></YearByYearComparision>
+          <SubCategoryMonthlyAnalysis
+            isExpense={false}
+          ></SubCategoryMonthlyAnalysis>
         </div>
       </NavMenu>
     </div>
