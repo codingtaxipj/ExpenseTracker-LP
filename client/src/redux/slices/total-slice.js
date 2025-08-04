ec01261b7ec2c7d5a3dd6bae0a466f669e9a813a
Prayas Jadli <prayasjadli18@gmail.com>
2025-08-04 20:38:14 +0530

    minor

--- client/src/redux/slices/total-slice.js
+++ client/src/redux/slices/total-slice.js
@@ -13,7 +13,7 @@ export const fetchTotal = createAsyncThunk("total/fetchTotal", async () => {
   const res = await axios.get(
     `http://127.0.0.1:8080/total/get-total/${userID}`,
   );
-  console.log("slice", res.data);
+
   return res.data;
 });
 
