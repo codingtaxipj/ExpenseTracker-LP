9352ec03ebcae8a80007789023c332657635a77d
Prayas Jadli <prayasjadli18@gmail.com>
2025-10-12 23:22:17 +0530

    starting profile pic ....

---
+++ client/src/components/avatar-upload.jsx
@@ -0,0 +1,11 @@
+// For selecting the file
+import { useDropzone } from "react-dropzone";
+// For cropping the image
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css"; // Don't forget the styles
+
+const AvatarUpload = () => {
+  return <></>;
+};
+
+export default AvatarUpload;
